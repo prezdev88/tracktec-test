90bf5ec2a7f30c407f55f69aff90a94c510e3a54
Patricio Pérez Pinto <patodeath@gmail.com>
2025-02-09 23:20:45 -0300

    feat: add imei encoded endpoint

--- src/main/java/cl/track/tec/test/five/model/EncodedImeiResponse.java
+++ src/main/java/cl/track/tec/test/five/model/EncodedImeiResponse.java
@@ -5,7 +5,7 @@ public class EncodedImeiResponse {
     private String imei;
 
     public EncodedImeiResponse(String imei) {
-        this.imei = imei;
+        this.imei = fillWithZerosToLength(imei);
     }
 
     public String getImei() {
@@ -14,5 +14,18 @@ public class EncodedImeiResponse {
 
     public void setImei(String imei) {
         this.imei = imei;
+    }
+
+    private String fillWithZerosToLength(String encoded) {
+        int encodedLength = encoded.length();
+
+        if (encodedLength >= 15) {
+            return encoded;
+        }
+
+        int zeroCount = 15 - encodedLength;
+        String zeros = "0".repeat(zeroCount);
+
+        return zeros + encoded;
     }
 }
--- src/main/java/cl/track/tec/test/five/service/impl/EncodeImeiServiceImpl.java
+++ src/main/java/cl/track/tec/test/five/service/impl/EncodeImeiServiceImpl.java
@@ -2,13 +2,37 @@ package cl.track.tec.test.five.service.impl;
 
 import cl.track.tec.test.five.model.EncodedImeiResponse;
 import cl.track.tec.test.five.service.EncodeImeiService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class EncodeImeiServiceImpl implements EncodeImeiService {
 
+    private HashMap<Character, Integer> letters;
+
+    @PostConstruct
+    public void init() {
+        letters = new HashMap<>();
+
+        int index = 1;
+        for (int i = 97; i <= 122; i++) {
+            letters.put((char) i, index++);
+        }
+    }
+
     @Override
     public EncodedImeiResponse encode(String imei) {
-        return new EncodedImeiResponse("000123");
+        imei = imei.toLowerCase();
+
+        StringBuilder encoded = new StringBuilder();
+
+        for (int i = 0; i < imei.length(); i++) {
+            char letter = imei.charAt(i);
+            encoded.append(letters.get(letter));
+        }
+
+        return new EncodedImeiResponse(encoded.toString());
     }
 }
